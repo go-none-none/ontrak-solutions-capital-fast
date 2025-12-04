9e7937b8e221a850537d27b2b0eaf191afc34b69
Ralph Dayan <ralph@ontrak.co>
2025-12-04 23:19:12 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-// Placeholder file, this should be overridden by the generated code
-
+import React from 'react';
+import HeroSection from '../components/home/HeroSection';
+import WhyChooseUs from '../components/home/WhyChooseUs';
+import HowItWorks from '../components/home/HowItWorks';
+import Testimonials from '../components/home/Testimonials';
+import CTASection from '../components/home/CTASection';
 
 export default function Home() {
-
   return (
-    <div>
+    <div className="min-h-screen">
+      <HeroSection />
+      <WhyChooseUs />
+      <HowItWorks />
+      <Testimonials />
+      <CTASection />
     </div>
   );
 }
