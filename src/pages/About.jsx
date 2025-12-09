e3161ef2fbc678fd27ef51244dcb58886e1dac87
Ralph Dayan <ralph@ontrak.co>
2025-12-09 18:26:35 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -312,7 +312,7 @@ export default function About() {
 
       {/* Commitment */}
       <section className="py-24 bg-slate-50">
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
