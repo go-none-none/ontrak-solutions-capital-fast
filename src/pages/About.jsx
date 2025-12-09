1a81b20350b652cd88d631886089a152b71f2933
Ralph Dayan <ralph@ontrak.co>
2025-12-09 18:28:45 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -108,7 +108,7 @@ export default function About() {
       </section>
 
       {/* CTA 1 */}
-      <section className="py-16 bg-white">
+      <section className="py-12 bg-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
