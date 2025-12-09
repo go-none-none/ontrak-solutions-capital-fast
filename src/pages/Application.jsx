f3fd8c8e725488480b13ce01a5739243746c287e
Ralph Dayan <ralph@ontrak.co>
2025-12-09 20:41:32 +0000

    File changes

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -144,11 +144,11 @@ export default function Application() {
               </div>
             </div>
           </div>
-          </div>
-          </div>
-          </section>
+        </div>
+      </div>
+    </section>
 
-          <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+    <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
         var ifr = document.getElementById("JotFormIFrame-252957146872065");
         if (ifr) {
           var src = ifr.src;
