df62896ea353d47bc1fb8846f88de1d5bd29d1ae
Ralph Dayan <ralph@ontrak.co>
2025-12-10 19:22:40 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Navigation from './components/shared/Navigation';
 import Footer from './components/shared/Footer';
 
 export default function Layout({ children }) {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [pathname]);
+
+  useEffect(() => {
+    const handleLinkClick = () => {
+      setTimeout(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+      }, 0);
+    };
+
+    document.addEventListener('click', handleLinkClick);
+    return () => document.removeEventListener('click', handleLinkClick);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
