70b5c9f53b07a4f0356959e5ff3c8e6d387d0d59
Ralph Dayan <ralph@ontrak.co>
2025-12-10 18:04:31 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
   }, [pathname]);
 
   return (
