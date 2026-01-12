8165d05796efb59c94ae3d73d15f36f6b5573d38
Ralph Dayan <ralph@ontrak.co>
2026-01-12 17:50:01 +0000

    File changes

--- src/components/rep/MiniDialer.jsx
+++ src/components/rep/MiniDialer.jsx
@@ -617,7 +617,7 @@ export default function MiniDialer({ session }) {
                     ))
                   )}
                 </div>
-              ))}
+              )}
             </div>
           </div>
         )}
