79f7ce5b5d7e0bb013869af2b8fe99a93f53e73c
Ralph Dayan <ralph@ontrak.co>
2026-01-14 22:03:10 +0000

    File changes

--- src/components/rep/RepPortalHeader.jsx
+++ src/components/rep/RepPortalHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { Home, Shield, RefreshCw, LogOut, Plus } from 'lucide-react';
+import { Home, Shield, RefreshCw, LogOut, Plus, ArrowLeft } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 
