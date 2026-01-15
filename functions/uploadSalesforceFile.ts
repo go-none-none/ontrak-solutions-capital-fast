5830eea5a04bf89485a89b3225a556e4eb57f6cf
Ralph Dayan <ralph@ontrak.co>
2026-01-15 18:36:37 +0000

    File changes

--- functions/uploadSalesforceFile.ts
+++ functions/uploadSalesforceFile.ts
@@ -1,8 +1,5 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-
 Deno.serve(async (req) => {
   try {
-    const base44 = createClientFromRequest(req);
     const body = await req.json();
     
     const { fileName, fileData, recordId, token, instanceUrl } = body;
