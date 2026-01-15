3d3ad6961182c7a31914e5e27b8e9e1df5e344d9
Ralph Dayan <ralph@ontrak.co>
2026-01-15 18:37:29 +0000

    File changes

--- functions/deleteSalesforceFile.ts
+++ functions/deleteSalesforceFile.ts
@@ -1,8 +1,5 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-
 Deno.serve(async (req) => {
   try {
-    const base44 = createClientFromRequest(req);
     const body = await req.json();
     
     const { contentDocumentId, token, instanceUrl } = body;
