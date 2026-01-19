500ea80fccccfa47529a2115aa03fd4e116f0dbb
Ralph Dayan <ralph@ontrak.co>
2026-01-19 20:02:28 +0000

    File changes

--- functions/deleteSalesforceRecord.ts
+++ functions/deleteSalesforceRecord.ts
@@ -1,15 +1,6 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-
 Deno.serve(async (req) => {
   try {
     const { objectType, recordId, token, instanceUrl } = await req.json();
-
-    const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user) {
-      return Response.json({ error: 'Unauthorized' }, { status: 401 });
-    }
 
     const response = await fetch(`${instanceUrl}/services/data/v59.0/sobjects/${objectType}/${recordId}`, {
       method: 'DELETE',
