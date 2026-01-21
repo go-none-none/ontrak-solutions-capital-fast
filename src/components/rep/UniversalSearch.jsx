a3a4f1f724065ff2f0a858653c0d14331f67c9bb
Ralph Dayan <ralph@ontrak.co>
2026-01-21 20:44:38 +0000

    File changes

--- src/components/rep/UniversalSearch.jsx
+++ src/components/rep/UniversalSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Search, Loader2, Contact, Briefcase, Building2, X, User, ChevronDown } from 'lucide-react';
+import { Search, Loader2, Contact, Briefcase, Building2, X, User } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
