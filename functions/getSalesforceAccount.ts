929b7edff427679021ba155a8cf5c30d5bf74fef
Ralph Dayan <ralph@ontrak.co>
2026-01-21 23:18:37 +0000

    File changes

--- functions/getSalesforceAccount.ts
+++ functions/getSalesforceAccount.ts
@@ -13,7 +13,7 @@ Deno.serve(async (req) => {
       return Response.json({ error: 'Missing required parameters' }, { status: 400 });
     }
 
-    const query = `SELECT Id, Name, Phone, Fax, Website, BillingStreet, BillingCity, BillingState, BillingPostalCode, BillingCountry, ShippingStreet, ShippingCity, ShippingState, ShippingPostalCode, ShippingCountry, Industry, NumberOfEmployees, Description, AccountNumber, Site, AnnualRevenue, RecordTypeId, RecordType.Name, CreatedBy.Name, Owner.Name, LastModifiedBy.Name, Parent.Name, csbs__Additional_Phone__c, csbs__Email__c, csbs__DBA__c, AccountSource, csbs__Type_of_Business__c, csbs__Product_Service_Sold__c, Sic, csbs__Tier__c, csbs__Tier_Position__c, csbs__Emails_to_CC__c, csbs__Emails_to_BCC__c, csbs__Minimum_Credit_Score__c, csbs__Maximum_Negative_Days__c, csbs__Minimum_Monthly_Deposit_Count__c, csbs__Maximum_NSFs__c, csbs__Minimum_Monthly_Deposit_Amount__c, csbs__Minimum_Average_Daily_Balance__c, csbs__Minimum_Months_in_Business__c, csbs__Maximum_Offer_Amount__c, csbs__Net_Offer_Percentage__c, csbs__Restricted_States__c, csbs__Restricted_Industries__c FROM Account WHERE Id = '${accountId}'`;
+    const query = `SELECT Id, Name, Phone, Fax, Website, BillingStreet, BillingCity, BillingState, BillingPostalCode, BillingCountry, ShippingStreet, ShippingCity, ShippingState, ShippingPostalCode, ShippingCountry, Industry, NumberOfEmployees, Description, AccountNumber, Site, AnnualRevenue, RecordTypeId, RecordType.Name, CreatedBy.Name, Owner.Name, LastModifiedBy.Name, Parent.Name FROM Account WHERE Id = '${accountId}'`;
 
     console.log('Querying Salesforce:', query);
 
